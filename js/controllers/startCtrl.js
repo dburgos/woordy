227c9a3d2d05e0daaa5834ba02d595b0dfd7323a
dburgos <david.burgos.risco@gmail.com>
2015-04-04 00:53:20 +0200

    Start up with AngularJS

---
+++ js/controllers/startCtrl.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('woordyApp')
+    .controller('startCtrl', ['$scope', startCtrl]);
+
+  function startCtrl($scope) {
+    'use strict';
+    // ready
+  }
+})();
