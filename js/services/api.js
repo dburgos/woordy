de694c6fa68aa87452798fd96374e4506494caa8
dburgos <david.burgos.risco@gmail.com>
2015-04-07 20:59:54 +0200

    Fix classname and API service output

--- js/services/api.js
+++ js/services/api.js
@@ -4,48 +4,50 @@
       .module('woordyApp',[])
       .factory('API',['$http','CREDENTIALS',function($http,CREDENTIALS){
       return {
-        getAll:function(){
-          return $http.get('https://api.parse.com/1/classes/Words',{
-            headers:{
-              'X-Parse-Application-Id': CREDENTIALS.APP_ID,
-              'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
-            }
-          });
-        },
-        get:function(id){
-          return $http.get('https://api.parse.com/1/classes/Words/'+id,{
-            headers:{
-              'X-Parse-Application-Id': CREDENTIALS.APP_ID,
-              'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
-            }
-          });
-        },
-        create:function(data){
-          return $http.post('https://api.parse.com/1/classes/Words',data,{
-            headers:{
+        Words: {
+          getAll:function(){
+            return $http.get('https://api.parse.com/1/classes/Word',{
+              headers:{
+                'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+              }
+            });
+          },
+          get:function(id){
+            return $http.get('https://api.parse.com/1/classes/Word/'+id,{
+              headers:{
+                'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+              }
+            });
+          },
+          create:function(data){
+            return $http.post('https://api.parse.com/1/classes/Word',data,{
+              headers:{
+                  'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                  'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+                  'Content-Type':'application/json'
+                }
+            });
+          },
+          edit:function(id,data){
+            return $http.put('https://api.parse.com/1/classes/Word/'+id,data,{
+              headers:{
                 'X-Parse-Application-Id': CREDENTIALS.APP_ID,
                 'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
                 'Content-Type':'application/json'
               }
-          });
-        },
-        edit:function(id,data){
-          return $http.put('https://api.parse.com/1/classes/Words/'+id,data,{
-            headers:{
-              'X-Parse-Application-Id': CREDENTIALS.APP_ID,
-              'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
-              'Content-Type':'application/json'
-            }
-          });
-        },
-        delete:function(id){
-          return $http.delete('https://api.parse.com/1/classes/Words/'+id,{
-            headers:{
-              'X-Parse-Application-Id': CREDENTIALS.APP_ID,
-              'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
-              'Content-Type':'application/json'
-            }
-          });
+            });
+          },
+          delete:function(id){
+            return $http.delete('https://api.parse.com/1/classes/Word/'+id,{
+              headers:{
+                'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+                'Content-Type':'application/json'
+              }
+            });
+          }
         }
       }
   }]).value('CREDENTIALS',{
