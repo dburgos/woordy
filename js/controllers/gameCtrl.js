f9ae7d8e71b661397bc1c01f95039106b7774f70
dburgos <david.burgos.risco@gmail.com>
2015-04-07 21:09:57 +0200

    Load the post via API

--- js/controllers/gameCtrl.js
+++ js/controllers/gameCtrl.js
@@ -3,14 +3,19 @@
 
   angular
     .module('woordyApp')
-    .controller('gameCtrl', ['$scope', gameCtrl]);
+    .controller('gameCtrl', ['$scope', 'API', gameCtrl]);
 
-  function gameCtrl($scope) {
+  function gameCtrl($scope, API) {
     'use strict';
     /* jshint validthis: true */
-		var vm = this;
-		vm.status = "new";
+    var vm = this;
+    vm.status = "new";
+
+    API.Words.getAll().success(function(data) {
+      vm.wordList = [];
+      if(data && data.results && data.results.length > 0) {
+        vm.wordList = data.results;
+      };
+    });
   }
-
-
 })();
