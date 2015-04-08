e853f0ba4a987b0588a35a80cee3b5f7754e9e44
dburgos <david.burgos.risco@gmail.com>
2015-04-08 22:46:52 +0200

    Add Score resource to API service

--- js/services/api.js
+++ js/services/api.js
@@ -48,6 +48,51 @@
               }
             });
           }
+        },
+        Scores: {
+          getAll:function(){
+            return $http.get('https://api.parse.com/1/classes/Score',{
+              headers:{
+                'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+              }
+            });
+          },
+          get:function(id){
+            return $http.get('https://api.parse.com/1/classes/Score/'+id,{
+              headers:{
+                'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+              }
+            });
+          },
+          create:function(data){
+            return $http.post('https://api.parse.com/1/classes/Score',data,{
+              headers:{
+                  'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                  'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+                  'Content-Type':'application/json'
+                }
+            });
+          },
+          edit:function(id,data){
+            return $http.put('https://api.parse.com/1/classes/Score/'+id,data,{
+              headers:{
+                'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+                'Content-Type':'application/json'
+              }
+            });
+          },
+          delete:function(id){
+            return $http.delete('https://api.parse.com/1/classes/Score/'+id,{
+              headers:{
+                'X-Parse-Application-Id': CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key': CREDENTIALS.REST_API_KEY,
+                'Content-Type':'application/json'
+              }
+            });
+          }
         }
       }
   }]).value('CREDENTIALS',{
